1546b53b98a74350a2c5f7d6c46f7613a6c0c1ff
limpopo113 <limpopo113@gmail.com>
2024-11-27 14:09:24 +0300

    first commit

---
+++ app/components/form.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+
+interface ITaskItem {
+    id: string
+    name: string
+    createAtAdd: string
+}
+export default function FristCompomemtd(){
+    return (<div>
+        
+
+        
+    </div>)
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import FristCompomemtd from "./components/form";
+
+export default function Home() {
+  return (
+    <div>
+      <FristCompomemtd />
+    </div>
+  );
+}
