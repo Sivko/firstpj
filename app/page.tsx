2e1ca8ac603a52315666cd9e176afeee519ca6f9
Roman-Kashcheev <RomanKashcheev@gmail.com>
2025-01-16 17:27:27 +0300

    animation-first-step

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import HeaderDesctop from "./components/Header-desctop";
 import HeaderMobile from "./components/Header-mobile";
+import ViewTest from "./components/ViewTest";
 
 export default function Home() {
   return (
     <div>
       <HeaderDesctop/>
-      <br />
+      <ViewTest />
       <HeaderMobile />
     </div>
   );
